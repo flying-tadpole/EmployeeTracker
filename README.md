c8210071299a46fd1d64de3ff628ae95c202b23e
flyingtadpole <archer.curry@northwestern.edu>
2023-10-19 14:08:31 -0500

    added final functions, troubleshooting, and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # EmployeeTracker
+
+## Description
+
+This was created for a coding bootcamp.
+
+The goal was to create a command line interface to assist with employee records. We were to use javascript, mysql2, inquirer, and node to complete this task.
+
+## Installation
+
+This application requires Inquirer and mysql2. 
+
+Prior to use, run the schema file to create the database and tables. Seed file of prepopulated data is available as well. 
+
+To run, start index.js file with node and follow command line prompts. 
+
+[GitHub Repo Link:](https://github.com/flying-tadpole/EmployeeTracker)
+
+[Video Walkthrough Link:]()
+
+## Credits
+
+Extensive usage of library documentation and code snippets provided in prior bootcamp lessons.
+
+Thank you to the instructor team for code troubleshooting and providing feedback and suggestions.
--- index.js
+++ index.js
@@ -3,10 +3,7 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 // imports all SQL queries
-const { seeDepartments, seeEmployees, seeRoles, sqlAddDepart, sqlAddEmployee, sqlAddRole, sqlBudget, sqlUpdateManager, departList } = require('./lib/queries');
-
-// imports main inquirer question sets
-const { mainMenu } = require('./lib/userChoices')
+const { seeDepartments, seeEmployees, seeRoles, sqlAddDepart, sqlAddEmployee, sqlAddRole, sqlBudget, departList, empList, roleList, sqlUpdateEmployee } = require('./lib/queries');
 
 // Connect to database
 const db = mysql.createConnection(
@@ -80,10 +77,14 @@ const addDepartmentFunc = () => {
     })
 }
 
-// see if viewDepartFunc comes back as array and try to pass it in 
-// change items from userChoices to objects?
+// add a new role
 const addRoleFunc = () => {
-  inquirer
+  db.query(departList, (err, results) => {
+    if (err) {
+      console.log("error in generating department list")
+    } else {
+      departments = results.map(({id, department_name}) => ({name: department_name, value: id}))
+      inquirer
     .prompt(
       [
         {
@@ -95,28 +96,16 @@ const addRoleFunc = () => {
           type: "input",
           message: "What is the salary of the new role?",
           name: "newRoleSalary"
+        },
+        {
+          type: "list",
+          message: "What department is this role in?",
+          name: "newRoleDepart",
+          choices: departments
         }
-      ]
+    ]
     )
     .then((response) => {
-      const savedAnswer = [response.newRoleName, response.newRoleSalary]
-      db.query(departList, (err, results) => {
-        if (err) {
-          console.log("error in generating department list")
-        } else {
-          const departments = results.map(({id, department_name}) => ({name: department_name, value: id}))
-          .prompt(
-            [
-              {
-                type: "list",
-                message: "What department is this role in?",
-                name: "newRoleDepart",
-                choices: departments
-              }
-            ]
-          )
-        }
-      })
       const params = [
         response.newRoleName,
         response.newRoleSalary,
@@ -131,16 +120,118 @@ const addRoleFunc = () => {
         init()
       })
     })
+    }
+  })
 }
 
 // add new employee
 const addEmp = () => {
-  console.log('adding employee')
+  db.query(empList, (err, results) => {
+    if (err) {
+      console.log("error in generating manager list")
+    } else {
+      managers = results.map(({id, Employee}) => ({name: Employee, value: id}))
+      db.query(roleList, (err, results) => {
+        if (err) {
+          console.log('error in generating roles list')
+        } else {
+          roles = results.map(({id, job_title}) => ({name: job_title, value: id}))
+          inquirer
+          .prompt(
+            [
+              {
+                type: "input",
+                message: "What is the employee's first name?",
+                name: "newEmpFirstName"
+              },
+              {
+                type: "input",
+                message: "What is the employee's last name?",
+                name: "newEmpLastName"
+              },
+              {
+                type: "list",
+                message: "What is the employee's role?",
+                name: "newEmpRole",
+                choices: roles
+              },
+              {
+                type: "list",
+                message: "Who is the employee's manager?",
+                name: "newEmpManager",
+                choices: managers
+              }
+          ]
+          )
+          .then((response) => {
+            const params = [
+              response.newEmpFirstName,
+              response.newEmpLastName,
+              response.newEmpRole,
+              response.newEmpManager
+            ]
+            db.query(sqlAddEmployee, params, (err, results) => {
+              if (err) {
+                console.log('could not update employee role')
+              } else {
+                console.log('role updated')
+              }
+              init()
+            })
+          })
+        }
+      })
+    }
+  })
 }
 
 // update employee record
 const updateEmp = () => {
-  console.log('updating employee')
+  db.query(empList, (err, results) => {
+    if (err) {
+      console.log("error in generating employee list")
+    } else {
+      employees = results.map(({id, Employee}) => ({name: Employee, value: id}))
+      db.query(roleList, (err, results) => {
+        if (err) {
+          console.log('error in generating roles list')
+        } else {
+          roles = results.map(({id, job_title}) => ({name: job_title, value: id}))
+          inquirer
+          .prompt(
+            [
+              {
+                type: "list",
+                message: "Which employee would you like to update the role of?",
+                name: "empChoice",
+                choices: employees
+              },
+              {
+                type: "list",
+                message: "What is their new role?",
+                name: "updatedEmpRole",
+                choices: roles
+              }
+          ]
+          )
+          .then((response) => {
+            const params = [
+              response.updatedEmpRole,
+              response.empChoice
+            ]
+            db.query(sqlUpdateEmployee, params, (err, results) => {
+              if (err) {
+                console.log('could not update employee role')
+              } else {
+                console.log('role updated')
+              }
+              init()
+            })
+          })
+        }
+      })
+    }
+  })
 }
 
 // view utilized budget
@@ -153,12 +244,31 @@ const viewBudget = () => {
     }
     init()
   })
-}
+} 
 
 // begins program and acts as main menu
 function init() {
   inquirer
-  .prompt(mainMenu)
+  .prompt(
+    [
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "menu",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "View budget",
+          "Quit"
+        ]
+      }
+  ]
+  )
   .then ((response) => {
     switch (response.menu) {
       case "View all departments":
